5cfcd0a4d8b8864c322fb6b91e3badc291d9642c
Mark Kop <mk.marcelo.mk@gmail.com>
2019-09-11 10:15:19 -0300

    added some temporary inline css

--- products/src/components/Header.js
+++ products/src/components/Header.js
@@ -1,10 +1,29 @@
 import React from "react";
 
+// Convert these styles to proper CSS
+const style = {
+  background: "#707070 0% 0% no-repeat padding-box",
+  border: "1px solid #707070",
+  opacity: 1
+};
+
+const p1Stlye = {
+  top: "69",
+  left: "635px",
+  width: "227px",
+  height: "23px",
+  textAlign: "left",
+  font: "Regular 20px/24px Helvetica Neue",
+  letterSpacing: 0,
+  color: "#FFFFFF",
+  opacity: 1
+};
+
 const Header = () => {
   return (
-    <header>
+    <header style={style}>
       <div>
-        <p>uma seleção de produtos</p>
+        <p style={p1Stlye}>uma seleção de produtos</p>
         <p>especial para você</p>
         <p>
           Todos os produtos desta lista foram selecionados a partir da sua
