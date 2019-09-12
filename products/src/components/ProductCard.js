b8387ac5cbf809797c78fec0295510382dfd1850
Mark Kop <mk.marcelo.mk@gmail.com>
2019-09-12 19:21:16 -0300

    added formated price

--- products/src/components/ProductCard.js
+++ products/src/components/ProductCard.js
@@ -1,14 +1,31 @@
 import React from "react";
 
+const formatPrice = (price = 0) => {
+  return price.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL"
+  });
+};
+
 const ProductCard = ({ product }) => {
+  formatPrice();
+
   return (
     <div className="productCard">
       <img alt={product.name} src={product.image} />
-      <p>{product.name}</p>
-      <p>{product.description}</p>
-      <p>De: {product.oldPrice}</p>
-      <p>Por: {product.price}</p>
-      <input type="button" value="Comprar" />
+      <div>
+        <p>{product.name}</p>
+        <p>{product.description}</p>
+      </div>
+      <div>
+        <p>De: {formatPrice(product.oldPrice)}</p>
+        <p>Por: {formatPrice(product.price)}</p>
+        <p>
+          ou {product.installments.count}x de{" "}
+          {formatPrice(product.installments.value)}
+        </p>
+      </div>
+      <input className="buyButton" type="button" value="Comprar" />
     </div>
   );
 };
