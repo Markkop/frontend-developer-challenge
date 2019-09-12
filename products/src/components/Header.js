cf2d443f49e981d0d7d4a000a02fcbd00146160c
Mark Kop <mk.marcelo.mk@gmail.com>
2019-09-12 17:22:40 -0300

    completed header css, except font-family

--- products/src/components/Header.js
+++ products/src/components/Header.js
@@ -1,28 +1,9 @@
 import React from "react";
 
-// Convert these styles to proper CSS
-const style = {
-  background: "#707070 0% 0% no-repeat padding-box",
-  border: "1px solid #707070",
-  opacity: 1
-};
-
-const p1Stlye = {
-  top: "69",
-  left: "635px",
-  width: "227px",
-  height: "23px",
-  textAlign: "left",
-  font: "Regular 20px/24px Helvetica Neue",
-  letterSpacing: 0,
-  color: "#FFFFFF",
-  opacity: 1
-};
-
 const Header = () => {
   return (
-    <header >
-      <div className="headerTexts">
+    <header>
+      <div>
         <p>uma seleção de produtos</p>
         <p>especial para você</p>
         <p>
@@ -30,7 +11,7 @@ const Header = () => {
           navegação. Aproveite!
         </p>
       </div>
-      <div className="headerButtons">
+      <div>
         <input type="button" value="Conheça a Linx" />
         <input type="button" value="Ajude o algorítimo" />
         <input type="button" value="Seus produtos" />
