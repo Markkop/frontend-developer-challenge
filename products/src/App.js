c4896386ba2a2a0abed5900fd1cdfb377e9ed644
Mark Kop <mk.marcelo.mk@gmail.com>
2019-09-13 15:53:30 -0300

    added size tool -to finish

--- products/src/App.js
+++ products/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "./components/organisms/Header";
 import Footer from "./components/organisms/Footer";
 import Content from "./components/templates/Content";
+import SizeTool from "./components/molecule/SizeTool";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <SizeTool />
       <Header />
       <Content />
       <Footer />
