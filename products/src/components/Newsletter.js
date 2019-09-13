3a00164493bb780b7414d1e734e0bb32cf00af1a
Mark Kop <mk.marcelo.mk@gmail.com>
2019-09-12 23:23:03 -0300

    added form hook and validation

--- products/src/components/Newsletter.js
+++ products/src/components/Newsletter.js
@@ -1,6 +1,14 @@
 import React from "react";
+import useForm from "../Hooks/useForm";
 
 const Newsletter = () => {
+  const registerNewsletter = () => {
+    // Add register newsletter function here
+    alert(JSON.stringify(values));
+  };
+
+  const { values, handleChange, handleSubmit } = useForm(registerNewsletter);
+
   return (
     <section>
       <h2>Compartilhe a novidade</h2>
@@ -8,16 +16,16 @@ const Newsletter = () => {
         Quer que seus amigos também ganhem a lista personalizada deles? Preencha
         agora!
       </p>
-      <form>
+      <form onSubmit={handleSubmit}>
         {/* Convert this input to component */}
         <div className="inputContainer">
           <div className="inputField">
             <label htmlFor="name">Nome do amigo:</label>
-            <input type="text" id="name" />
+            <input type="text" id="name" onChange={handleChange} required />
           </div>
           <div className="inputField">
             <label htmlFor="email">Email do amigo:</label>
-            <input type="text" id="email" />
+            <input type="email" id="email" onChange={handleChange} required />
           </div>
         </div>
         <input className="button" type="submit" value="Enviar agora" />
